01c287c76d27aae3c916e1a7a5741ace78a958c2
David <chickensinspace@gmail.com>
2020-12-15 20:28:42 -0600

    start making basic test objects

---
+++ OhHellUnityProject/Assets/OhHell/Networking/NetworkManagerOhHell.cs
@@ -0,0 +1,36 @@
+﻿using Mirror;
+using UnityEngine;
+
+[AddComponentMenu("")]
+public class NetworkManagerOhHell : NetworkManager
+{
+    public Transform leftRacketSpawn;
+    public Transform rightRacketSpawn;
+    GameObject ball;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        // add player at correct spawn position
+        Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
+        GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+        player.GetComponent<PlayerOhHell>().CreateDialog(numPlayers);
+        // spawn ball if two players
+        if (numPlayers == 2)
+        {
+            ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+            NetworkServer.Spawn(ball);
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        // destroy ball
+        if (ball != null)
+            NetworkServer.Destroy(ball);
+
+        // call base functionality (actually destroys the player)
+        base.OnServerDisconnect(conn);
+    }
+}
+
---
+++ OhHellUnityProject/Assets/OhHell/Networking/PlayerOhHell.cs
@@ -0,0 +1,28 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOhHell : NetworkBehaviour
+{
+    public float speed = 30;
+    public Rigidbody2D rigidbody2d;
+    public GameObject test;
+
+    // need to use FixedUpdate for rigidbody
+    void FixedUpdate()
+    {
+        // only let the local player control the racket.
+        // don't control other player's rackets
+        if (isLocalPlayer)
+            rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+    }
+
+    [ClientRpc]
+    public void CreateDialog(int amount)
+    {
+        Debug.Log("Took damage:" + amount);
+        GameObject.Instantiate(test, transform);
+        GameObject.Instantiate(test);
+    }
+}
