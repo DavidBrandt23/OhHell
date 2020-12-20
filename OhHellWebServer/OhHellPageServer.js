07261af80ad3a7162b0c43e7a02ea3204446c601
David <chickensinspace@gmail.com>
2020-12-20 16:12:39 -0600

    add basic nodejs web server

---
+++ OhHellWebServer/OhHellPageServer.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
+
+var staticBasePath = './static';
+
+var staticServe = function(req, res) {
+    var resolvedBase = path.resolve(staticBasePath);
+    var safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '');
+    var fileLoc = path.join(resolvedBase, safeSuffix);
+    
+    fs.readFile(fileLoc, function(err, data) {
+        if (err) {
+            res.writeHead(404, 'Not Found');
+            res.write('404: File Not Found!');
+            return res.end();
+        }
+        
+        res.statusCode = 200;
+
+        res.write(data);
+        return res.end();
+    });
+};
+
+var httpServer = http.createServer(staticServe);
+
+httpServer.listen(8080);
